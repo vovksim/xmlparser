bb505d7301382d4851d3b03179cbd24660016579
Ivan Mykhailov <ivan.mykhailov11@gmail.com>
2024-10-25 00:11:20 +0300

    Changed DOMParser implementation according to new mapAttributes() method.

--- src/main/java/com/parser/DOM/DOMParser.java
+++ src/main/java/com/parser/DOM/DOMParser.java
@@ -1,6 +1,7 @@
 package com.parser.DOM;
 
 import org.w3c.dom.Document;
+import org.w3c.dom.NamedNodeMap;
 import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
 import org.xml.sax.SAXException;
@@ -10,6 +11,8 @@ import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
 import java.io.File;
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
 //parser can handle different xml files with:
 //1.simple/complex type
@@ -33,8 +36,15 @@ public class DOMParser {
             if (childNode.getNodeType() == Node.ELEMENT_NODE) {
                 if (childNode.getChildNodes().getLength() > 0)
                     parse(childNode);
-                if (childNode.hasAttributes())
-                    mapper.mapAttributes(childNode);
+                if (childNode.hasAttributes()) {
+                    NamedNodeMap attributes = childNode.getAttributes();
+                    Map<String, String> attributeNameValueMap = new HashMap<>();
+                    for (int j = 0; j < attributes.getLength(); j++) {
+                        Node attribute = attributes.item(j);
+                        attributeNameValueMap.put(attribute.getNodeName(), attribute.getNodeValue());
+                    }
+                    mapper.mapAttributes(childNode.getNodeName(), attributeNameValueMap);
+                }
             }
             if (childNode.getNodeType() == Node.TEXT_NODE) {
                 mapper.mapValue(childNode.getParentNode().getNodeName(), childNode.getNodeValue());
