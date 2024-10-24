3efed8f8597d95a652a955b844fe0452a5819154
Ivan Mykhailov <ivan.mykhailov11@gmail.com>
2024-10-25 01:16:53 +0300

    Demonstration of using each parser.

--- src/main/java/com/edu/Main.java
+++ src/main/java/com/edu/Main.java
@@ -1,17 +1,44 @@
 package com.edu;
 
+import com.parser.DOM.AbstractDOMStAXHandler;
+import com.parser.DOM.DOMParser;
+import com.parser.DOM.DeviceDOMStAXHandler;
+import com.parser.SAX.SAXDeviceHandler;
+import com.parser.StAX.StAXParser;
+import com.structure.Device;
+import com.util.XMLValidator;
+import org.xml.sax.SAXException;
+
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.parsers.SAXParser;
+import javax.xml.parsers.SAXParserFactory;
+import javax.xml.stream.XMLStreamException;
+import java.io.IOException;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.println("Hello and welcome!!!!!!!");
+    static final String DATA_PATH = "test1instance.xml";
+    static final String SCHEMA_PATH = "devices.xsd";
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+    public static void main(String[] args) throws ParserConfigurationException, IOException, SAXException, XMLStreamException {
+        XMLValidator.validate(SCHEMA_PATH, DATA_PATH);
+
+        DOMParser parser = new DOMParser();
+        AbstractDOMStAXHandler<Device> deviceDOMHandler = new DeviceDOMStAXHandler();
+
+        parser.parse(DATA_PATH, deviceDOMHandler);
+        System.out.println(deviceDOMHandler.getResult());
+
+
+        SAXParserFactory saxParserFactory = SAXParserFactory.newInstance();
+        SAXParser saxParser = saxParserFactory.newSAXParser();
+        SAXDeviceHandler saxDeviceHandler = new SAXDeviceHandler();
+        saxParser.parse(DATA_PATH, saxDeviceHandler);
+        System.out.println(saxDeviceHandler.getResult());
+
+        StAXParser staxParser = new StAXParser();
+        staxParser.parse(DATA_PATH, deviceDOMHandler);
+        System.out.println(deviceDOMHandler.getResult());
     }
 }
